1a4e046cf60a9f5bf7d69c5aafe5a9d1987e40ea
elgrenka <bugigen@yandex.ru>
2023-06-03 09:29:02 +0400

    init

---
+++ database/factories/ArticleFactory.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Article;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Article>
+ */
+class ArticleFactory extends Factory
+{
+    protected $model = Article::class;
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        $title = $this->faker->sentence(6, true);
+        $slug = Str::substr(Str::lower(preg_replace('/\s+/', '-', $title)), 0, -1);
+        return [
+            'title' => $title,
+            'body' => $this->faker->paragraph(100, true),
+            'slug' => $slug,
+            'img' => 'https://ru.freepik.com/premium-psd/window-shop-mockup-design_39757803.htm#&position=5&from_view=category',
+            'created_at' => $this->faker->dateTimeBetween('-1 years'),
+        ];
+    }
+}
